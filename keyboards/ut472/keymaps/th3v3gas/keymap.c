a3a3b4e308ebccbecf06e9ac737405728a6a54a9
th3v3gas <61045367+th3v3gas@users.noreply.github.com>
2020-04-03 14:09:03 -0700

    ,m!.,.,.,.

--- keyboards/ut472/keymaps/th3v3gas/keymap.c
+++ keyboards/ut472/keymaps/th3v3gas/keymap.c
@@ -374,6 +374,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______,_______,_______,_______,_______,_______,_______,M_DSCRD,_______,_______,_______),
 };
 
+//per layer underglow
 void persistent_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
